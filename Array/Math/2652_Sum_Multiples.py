52e207ab78b86da37967f9de27d17b39397b6273
Rajesh Kanna S <srk8115939@gmail.com>
2025-06-12 19:47:43 +0530

    Delete Array/Math directory

--- Array/Math/2652_Sum_Multiples.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def sumOfMultiples(self, n):
-        res=0
-        for i in range(1,n+1):
-            if i%3==0 or i%5==0 or i%7==0:
-                res+=i
-        return res
-sol=Solution()
-print(sol.sumOfMultiples(7))
